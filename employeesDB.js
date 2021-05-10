fa11fd101e5716627982b0a4785a4325ff730fe1
ryanascherr <ryanascherr@gmail.com>
2021-05-10 09:14:17 -0400

    Added function for viewing department table.

--- employeesDB.js
+++ employeesDB.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -14,8 +15,48 @@ const connection = mysql.createConnection({
   database: 'employees_db',
 });
 
+const start = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "answer",
+            message: "How to you want to do?",
+            choices: ["Add a department", "Add a role", "Add an employee", "View departments", "View roles", "View employees"],
+        }
+    ]).then(({answer}) => {
+        switch(answer) {
+            case "Add a department":
+                addDepartment();
+                break;
+            case "Add a role":
+                addRole();
+                break;
+            case "Add a employee":
+                addEmployee();
+                break;
+            case "View departments":
+                viewDepartments();
+                break;
+            case "View roles":
+                viewRoles();
+                break;
+            case "View employees":
+                viewEmployees();
+                break;
+        }
+    })
+}
+
+const viewDepartments = () => {
+    connection.query('SELECT * FROM department', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+    })
+}
+
 connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}`);
-    // createPlaylist();
+    start();
   });
+
